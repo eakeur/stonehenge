99bda1d2f2af679004e43af68974da25f34bd4be
Igor <igor.reisleandro@gmail.com>
2021-11-10 21:11:30 +0000

    updated core model

---
+++ core/model/errors.go
@@ -0,0 +1,21 @@
+package model
+
+import "errors"
+
+var (
+	ErrCreating       = errors.New("ops! an error occurred while adding your stuff. please, try again later")
+	ErrAccountInvalid = errors.New("ops! the account id provided is invalid. please, try again with a valid one")
+	ErrLoginInvalid   = errors.New("ops! the cpf provided does not have an account yet. come on, register yourself, you'll love it")
+	ErrInvalidBody    = errors.New("ops! it seems like the json provided in the request body is invalid. please check if you typed everything correctly")
+	ErrCPFInvalid     = errors.New("ops! the cpf provided is invalid. please, try again with a valid cpf")
+	ErrUnauthorized   = errors.New("ops! to access this resource, you have to be logged in. please, log in or create a new account")
+	ErrForbidden      = errors.New("ops! you don't have access to this resource. please, log in with an account that has it")
+	ErrWrongPassword  = errors.New("ops! wrong password. please check if you typed everything right")
+	ErrPostTransfer   = errors.New("ops! an error occurred while transfering your money. please be sure if the amount has left your account or contact us")
+	ErrNoMoney        = errors.New("account does not have money enough to withdraw")
+	ErrAmountInvalid  = errors.New("ops! the value you requested to transfer is invalid. a transfer must have a value bigger than 0")
+	ErrAccountExists  = errors.New("ops! it seems like there is already an account with this cpf. please, try logging in")
+	ErrSameTransfer   = errors.New("ops! it seems like you want to transfer money to yourself. you know that that's not gonna make you rich")
+	ErrInternal       = errors.New("ops! an internal error occurred while processing your request. please, try again later")
+	ErrNotFound       = errors.New("ops! We could not find the requested item or it does not exist. what about checking if you typed everything right?")
+)
---
+++ core/model/transfer.go
@@ -0,0 +1,69 @@
+package model
+
+import (
+	"time"
+)
+
+// Transfer hold useful information about transaction between accounts
+type Transfer struct {
+	// The unique identifier of this entity
+	Id string `json:"id"`
+
+	// The id of the origin account
+	AccountOriginId string `json:"account_origin_id"`
+
+	// The id of the destination account
+	AccountDestinationId string `json:"account_destination_id"`
+
+	// The value to be transacted
+	Amount int64 `json:"amount"`
+
+	// The time the transfer has been made
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// Tranfers money from an account to the other
+func (t *Transfer) TransferMoney(origin *Account, destination *Account) error {
+	t.AccountOriginId = origin.Id
+	t.AccountDestinationId = destination.Id
+
+	if t.Amount <= 0 {
+		return ErrAmountInvalid
+	}
+
+	if t.AccountDestinationId == t.AccountOriginId {
+		return ErrSameTransfer
+	}
+
+	// Tries to widthdraw the origin account's money, if there is any
+	_, withdrawalError := origin.Withdraw(t.Amount)
+	if withdrawalError != nil {
+		return withdrawalError
+	}
+	// Deposits the amount to the destination account
+	destination.Deposit(t.Amount)
+
+	return nil
+}
+
+// Returns a map of this transfer instance
+func (t *Transfer) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"id":                     t.Id,
+		"account_origin_id":      t.AccountOriginId,
+		"account_destination_id": t.AccountDestinationId,
+		"amount":                 t.Amount,
+		"created_at":             t.CreatedAt,
+	}
+}
+
+// Returns an instance of a transfer based on the data passed as parameter
+func TransferFromMap(mapInput map[string]interface{}) Transfer {
+	return Transfer{
+		Id:                   mapInput["id"].(string),
+		AccountOriginId:      mapInput["account_origin_id"].(string),
+		AccountDestinationId: mapInput["account_destination_id"].(string),
+		Amount:               mapInput["amount"].(int64),
+		CreatedAt:            mapInput["created_at"].(time.Time),
+	}
+}
