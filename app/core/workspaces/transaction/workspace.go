432085019e2769496bfa2da0c711a5a646c37dda
Igor <igor.reisleandro@gmail.com>
2021-12-14 21:38:08 -0300

    created transaction workspace contract

---
+++ app/core/workspaces/transaction/workspace.go
@@ -0,0 +1,25 @@
+package transaction
+
+import (
+	"context"
+	"stonehenge/app/core/model/account"
+	"stonehenge/app/core/model/transfer"
+	"stonehenge/app/core/types/currency"
+)
+
+type Workspace interface {
+	Transfer(ctx context.Context, req TransferRequest)
+	Withdraw(ctx context.Context, req WithdrawalRequest) (currency.Currency, error)
+	Deposit(ctx context.Context, req DepositRequest) (currency.Currency, error)
+}
+type workspace struct {
+	ac account.Repository
+	tr transfer.Repository
+}
+
+func New(ac account.Repository, tr transfer.Repository) *workspace {
+	return &workspace{
+		ac: ac,
+		tr: tr,
+	}
+}
