e6534fd44a7439ec2d35b8a4b9f8db8edb8a0994
Igor <igor.reisleandro@gmail.com>
2021-12-14 21:36:53 -0300

    created list usecase in transfer workspace

---
+++ app/core/workspaces/transfer/list.go
@@ -0,0 +1,44 @@
+package transfer
+
+import (
+	"context"
+	"stonehenge/app/core/model/transfer"
+	"stonehenge/app/core/types/currency"
+	"stonehenge/app/core/types/id"
+	"time"
+)
+
+type ListRequest struct {
+	Context context.Context
+	Filter  transfer.Filter
+}
+
+type Reference struct {
+	Id id.ID
+
+	OriginId id.ID
+
+	DestinationId id.ID
+
+	Amount currency.Currency
+
+	EffectiveDate time.Time
+}
+
+func (u *workspace) List(request ListRequest) ([]Reference, error) {
+	list, err := u.tr.List(request.Context, request.Filter)
+	if err != nil {
+		return nil, err
+	}
+	refs := make([]Reference, 0, len(list))
+	for i, a := range list {
+		refs[i] = Reference{
+			Id:            a.Id,
+			OriginId:      a.OriginId,
+			DestinationId: a.DestinationId,
+			Amount:        a.Amount,
+			EffectiveDate: a.EffectiveDate,
+		}
+	}
+	return refs, nil
+}
