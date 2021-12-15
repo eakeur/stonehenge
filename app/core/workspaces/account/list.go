4f6626b7025258c1dda3161e34df2ce06affd77a
Igor <igor.reisleandro@gmail.com>
2021-12-14 21:35:32 -0300

    created list usecase in account workspace

---
+++ app/core/workspaces/account/list.go
@@ -0,0 +1,32 @@
+package account
+
+import (
+	"context"
+	"stonehenge/app/core/model/account"
+	"stonehenge/app/core/types/id"
+)
+
+type ListRequest struct {
+	Context context.Context
+	Filter  account.Filter
+}
+
+type Reference struct {
+	Id   id.ID
+	Name string
+}
+
+func (u *workspace) List(ctx context.Context, filter account.Filter) ([]Reference, error) {
+	list, err := u.ac.List(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	refs := make([]Reference, 0, len(list))
+	for i, a := range list {
+		refs[i] = Reference{
+			Id:   a.Id,
+			Name: a.Name,
+		}
+	}
+	return refs, nil
+}
