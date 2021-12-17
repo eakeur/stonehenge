f7daccefe2655e7777d8f3c80b397fa380423273
Igor <igor.reisleandro@gmail.com>
2021-12-17 15:25:28 -0300

    created runners

---
+++ cmd/cli/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"stonehenge/app/gateway/api/server"
+	"stonehenge/app/gateway/database/postgres"
+)
+
+
+
+func main() {
+
+	ctx := context.Background()
+
+	connection, err := postgres.NewConnection(ctx, "postgres://postgres:postgres@localhost:5432/stonehenge?sslmode=disable", "/home/igor/go/src/stonehenge/app/gateway/database/postgres/migrations", nil, 5)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	connection.Query(ctx, "select * from accounts")
+
+	adapter := postgres.NewTransactionAdapter(connection)
+
+	repos := server.NewPostgresRepositoryWrapper(connection, adapter)
+
+	server.NewWorkspaceWrapper(repos)
+}
