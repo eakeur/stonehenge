e1f367d66409ebc3a2eaa4c90a8a33839f2c676e
Igor <igor.reisleandro@gmail.com>
2021-12-17 15:24:31 -0300

    created auth middleware

---
+++ app/gateway/api/middlewares/authorization/auth.go
@@ -0,0 +1,47 @@
+package authorization
+
+import (
+	"net/http"
+	"stonehenge/app/gateway/api/common"
+	"strings"
+)
+
+var Middleware = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, r *http.Request) {
+		token := getToken(r)
+		account, err := common.ExtractToken(token)
+		if err != nil {
+			wr.WriteHeader(http.StatusUnauthorized)
+			//fmt.Fprint(wr, erring.ErrUnauthorized.Error())
+			return
+		}
+
+		token, err = common.CreateToken(*account.AccountId)
+		if err != nil {
+			wr.WriteHeader(http.StatusUnauthorized)
+			//fmt.Fprint(wr, erring.ErrUnauthorized.Error())
+			return
+		}
+
+		common.AssignToken(wr, token)
+
+		ctx := common.AssignUserToContext(r.Context(), *account.AccountId)
+		next.ServeHTTP(wr, r.WithContext(ctx))
+	})
+}
+
+// getToken returns the request token present in the request
+func getToken(r *http.Request) string {
+	jwtToken := r.Header.Get("Authorization")
+	if strings.Trim(jwtToken, " ") == "" {
+		cookies := r.Cookies()
+		for _, cookie := range cookies {
+			if cookie.Name == "access_token" {
+				jwtToken = cookie.Value
+				break
+			}
+		}
+	}
+
+	return jwtToken
+}
