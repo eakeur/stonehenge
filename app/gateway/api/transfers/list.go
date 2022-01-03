7468552a466dfed78e4b183a11dbfcb81be3565d
Igor <igor.reisleandro@gmail.com>
2022-01-03 12:49:49 -0300

    feat: fixed side effects of type change

--- app/gateway/api/transfers/list.go
+++ app/gateway/api/transfers/list.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/url"
 	"stonehenge/app/core/model/transfer"
+	"stonehenge/app/core/types/id"
 	"stonehenge/app/gateway/api/common"
 	"stonehenge/app/gateway/api/responses"
 	"time"
@@ -12,7 +13,7 @@ import (
 // List gets all transfers of this actual account
 func (c *Controller) List(rw http.ResponseWriter, r *http.Request) {
 	filters := getFilter(r.URL.Query())
-	filters.OriginID = string(common.FetchContextUser(r.Context()))
+	filters.OriginID = common.FetchContextUser(r.Context())
 	list, err := c.workspace.List(r.Context(), filters)
 	if err != nil {
 		responses.WriteErrorResponse(rw, http.StatusBadRequest, err)
@@ -28,11 +29,11 @@ func (c *Controller) List(rw http.ResponseWriter, r *http.Request) {
 func getFilter(values url.Values) transfer.Filter {
 	filter := transfer.Filter{}
 
-	if ori := values.Get("origin"); ori != "" {
+	if ori := id.From(values.Get("origin")); ori != id.ZeroValue {
 		filter.OriginID = ori
 	}
 
-	if dest := values.Get("destination"); dest != "" {
+	if dest := id.From(values.Get("destination")); dest != id.ZeroValue {
 		filter.OriginID = dest
 	}
 
