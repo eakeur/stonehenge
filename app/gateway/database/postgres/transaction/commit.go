3b347c8f9d69ce3c31760d6d2e70313894299487
Igor <igor.reisleandro@gmail.com>
2022-01-06 16:47:28 -0300

    refac: put transaction errors in domain

--- app/gateway/database/postgres/transaction/commit.go
+++ app/gateway/database/postgres/transaction/commit.go
@@ -2,13 +2,14 @@ package transaction
 
 import (
 	"context"
+	"stonehenge/app/core/entities/transaction"
 	"stonehenge/app/gateway/database/postgres/common"
 )
 
 func (t *pgxTransaction) Commit(ctx context.Context) error {
 	tx, ok := common.TransactionFrom(ctx)
 	if !ok {
-		return ErrNoTransaction
+		return transaction.ErrNoTransaction
 	}
 
 	if err := tx.Commit(ctx); err != nil {
