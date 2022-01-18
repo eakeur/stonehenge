fa055c9a0797b9dcc3cf4164fcda08a2b7ca8c2c
Igor <igor.reisleandro@gmail.com>
2022-01-18 12:57:10 -0300

    style: changed log to track DB connection problems

--- app/gateway/database/postgres/postgrestest/test.go
+++ app/gateway/database/postgres/postgrestest/test.go
@@ -54,6 +54,7 @@ func createContainer() (func(), error) {
 
 	if err := pool.Retry(func() error {
 		db, err = connect(testUser, testPassword, testHost, port, testDatabase)
+		log.Printf("Error connecting to the database: %v", err)
 		return err
 	}); err != nil {
 		teardown()
@@ -70,11 +71,18 @@ func createResource(userName, databaseName, userPassword string) (*dockertest.Po
 		return pool, nil, errors.Wrap(err, "the docker pool connection could not be established")
 	}
 
+	if err := pool.Client.Ping(); err != nil {
+		return pool, nil, errors.Wrap(err, "could not contact docker pool")
+	}
+
 	resource, err := pool.Run("postgres", "latest", []string{
 		fmt.Sprintf("POSTGRES_USER=%s", userName),
 		fmt.Sprintf("POSTGRES_PASSWORD=%s", userPassword),
 		fmt.Sprintf("POSTGRES_DB=%s", databaseName),
 	})
+
+	log.Printf("Docker test container status: %v", resource.Container.State.Status)
+
 	if err != nil {
 		return pool, resource, errors.Wrap(err, "the docker container could not be created")
 	}
