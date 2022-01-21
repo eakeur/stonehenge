e4e298d520394121c3b4c88d3d3e41c8c4bd3a7b
Igor <igor.reisleandro@gmail.com>
2022-01-21 15:46:19 -0300

    feat: create parsing currency functions

--- app/core/types/currency/currency.go
+++ app/core/types/currency/currency.go
@@ -3,11 +3,13 @@ package currency
 // Currency is an implementation of an int64 with special functions to deal with money
 type Currency int64
 
-func New(input int64) Currency {
-	return Currency(input)
+func (c Currency) ToStandardCurrency() float64 {
+	return float64(c) / 100
 }
 
-func FromFloat64(input float64) Currency {
+func FromStandardCurrency(input float64) Currency {
 	output := int64(input * 100)
 	return Currency(output)
 }
+
+
