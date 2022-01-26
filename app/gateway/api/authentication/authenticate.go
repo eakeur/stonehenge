858c99d3524a49a7055aa0223da3d1f719b3efdc
Igor <igor.reisleandro@gmail.com>
2022-01-26 17:51:34 -0300

    feat: add logs to authentication controller

--- app/gateway/api/authentication/authenticate.go
+++ app/gateway/api/authentication/authenticate.go
@@ -15,20 +15,24 @@ type LoginRequestBody struct {
 
 // Authenticate logs an applicant in
 func (c *Controller) Authenticate(r *http.Request) rest.Response {
+	const operation = "Controller.Authentication.Authenticate"
+	ctx := r.Context()
 	body, err := schema.NewAuthenticationRequest(r.Body)
 	if err != nil {
+		c.logger.Error(ctx, operation, err.Error())
 		return rest.BuildErrorResult(err)
 	}
 
-	ctx := r.Context()
 	acc, err := c.workspace.Authenticate(ctx, authentication.AuthenticationRequest{
 		Document: document.Document(body.Document),
 		Secret:   body.Secret,
 	})
 	if err != nil {
+		c.logger.Error(ctx, operation, err.Error())
 		return rest.BuildErrorResult(err)
 	}
 
+	c.logger.Trace(ctx, operation, "finished process successfully")
 	return rest.
 		BuildOKResult(schema.AuthenticationResponse{Token: acc.Token}).
 		AddHeaders("Authorization", "Bearer "+acc.Token)
