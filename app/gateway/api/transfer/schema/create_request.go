61457473caece2d46c2b12c0fbff98eb30822d45
Igor <igor.reisleandro@gmail.com>
2022-01-30 10:15:42 -0300

    fix: corrected json tags to challenge standard

--- app/gateway/api/transfer/schema/create_request.go
+++ app/gateway/api/transfer/schema/create_request.go
@@ -7,7 +7,7 @@ import (
 
 type CreateRequest struct {
 	// DestinationID is the ID of the transfer that wil receive the money
-	DestinationID string `json:"destination_id" example:"123e4567-e89b-12d3-a456-426614174000"`
+	DestinationID string `json:"account_destination_id" example:"123e4567-e89b-12d3-a456-426614174000"`
 
 	// Amount is the quantity that is going to be attempted to transfer
 	Amount float64 `json:"amount" example:"15.99"`
--- app/gateway/api/transfer/schema/list_response.go
+++ app/gateway/api/transfer/schema/list_response.go
@@ -5,9 +5,9 @@ import (
 )
 
 type ListResponse struct {
-	ExternalID    string `json:"external_id"`
-	OriginID      string `json:"origin_id"`
-	DestinationID string `json:"destination_id"`
+	ExternalID    string `json:"id"`
+	OriginID      string `json:"account_origin_id"`
+	DestinationID string `json:"account_destination_id"`
 	Amount        float64 `json:"amount"`
 	EffectiveDate time.Time `json:"effective_date"`
 }
