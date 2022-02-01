d3101c674f3d6b01fd88c8c277fcc9aa0c96d461
Igor <igor.reisleandro@gmail.com>
2022-02-01 11:01:33 -0300

    feat: added wildcards to access control headers

---
+++ app/gateway/api/middlewares/cors.go
@@ -0,0 +1,14 @@
+package middlewares
+
+import (
+	"net/http"
+)
+
+func (m middleware) CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		header := rw.Header()
+		header.Add("Access-Control-Allow-Origin", "*")
+		header.Add("Access-Control-Allow-Methods", "*")
+		header.Add("Access-Control-Allow-Headers", "*")
+	})
+}
